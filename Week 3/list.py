a712e47512c41adb6f09951858e5dc849a46d782
Tushar Tandon <tushartandon99@gmail.com>
2026-03-28 14:21:15 +0530

    update

--- Week 3/list.py
+++ Week 3/list.py
@@ -14,4 +14,27 @@ shopping.append("curd")
 print(shopping)
 
 #insert
-shopping
+shopping.insert(0,"shampoo")
+print(shopping)
+
+#count
+ages=[12,23,34,42,15,87,12,16,12,25,23,24,23,20]
+print(ages.count(12))
+print(ages.count(23))
+print(ages.count(70))
+
+#len
+print(len(ages))
+print(len(shopping))
+
+#sort
+ages.sort()
+print(ages)
+
+#reverse
+ages.reverse()
+print (ages)
+
+students=["Arun","Rajesh","Harish","Akansha","Luxmi","Varsha"]
+students.sort()
+print(students)
