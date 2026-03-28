fe84397f8961ee81180baef9e2aa4b551f154c51
Tushar Tandon <tushartandon99@gmail.com>
2026-03-28 13:44:15 +0530

    neww

---
+++ Week 3/list.py
@@ -0,0 +1,17 @@
+t1="Amit"
+t2="Simran"
+t3="Vidhya"
+i="sudarshan"
+#list
+shopping=["Bread","coffee","sugar"]
+print(shopping)
+for i in range(3):
+    print (i)
+for item in shopping:
+    print (item)
+#append
+shopping.append("curd")
+print(shopping)
+
+#insert
+shopping
