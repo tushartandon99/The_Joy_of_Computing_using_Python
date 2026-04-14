7e055cda314cfcd6cfd61bf36160058a099d3cf5
Tushar Tandon <tushartandon99@gmail.com>
2026-04-14 11:54:54 +0530

    Week 8

--- Week 8/Facebook.py
+++ Week 8/Facebook.py
@@ -1,13 +1,21 @@
 import pandas as pd
 import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-nltk.downloader.download('vader_lexicon')
+nltk.download('vader_lexicon')
 
 file='D:\\coding\\python\\Joy of computing using python\\Week 8\\data.xlsx'
 
 xl=pd.ExcelFile(file)#read from excel
 dfs=xl.parse(xl.sheet_names[0])#parsing the excel sheet to data frame
-print(dfs.columns)  # check column names
-
-# dfs=list(dfs['Timeline'])#removes the blank lines
+dfs = dfs['Timeline'].dropna().astype(str).tolist()#removes the blank lines
 print(dfs)
+
+sid=SentimentIntensityAnalyzer()
+str1="UTC+05:30"
+for data in dfs:
+    a=data.find(str1)
+    if(a==-1):
+        ss=sid.polarity_scores(data)
+        print(data)
+        for k in ss:
+            print(k,ss[k])
